258b09b2b072abd4d33721382963bb67ddd17190
Shivagami3006 <104749417+Shivagami3006@users.noreply.github.com>
2023-03-01 12:13:22 +0530

    second

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const onLogout = () => {
+    localStorage.removeItem("usr");
+    dispatch(logout());
+    navigate("/login");
+  };
+
   return (
     <header className="header">
       <div className="logo">
@@ -10,13 +22,22 @@ function Header() {
       </div>
       <ul>
         <li>
-          <FaUser></FaUser>
+          <FaUser/>
           <Link to="/register">Register</Link>
         </li>
-        <li>
-          <FaSignInAlt></FaSignInAlt>
-          <Link to="/login">Login</Link>
-        </li>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt />
+              Logout
+            </button>
+          </li>
+        ) : (
+          <li>
+            <FaSignInAlt />
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
     </header>
   );
